a2e79c4aa518746fb7844e370071d5b4a1e1055f
Nicolas Matton <nicolas@locla.be>
2021-02-09 17:13:34 +0100

    updated colors + feedback form on modal window

---
+++ assets/php/form-to-email.php
@@ -0,0 +1,62 @@
+<?php
+if(!isset($_POST['submit']))
+{
+	//This page should not be accessed directly. Need to submit the form.
+	echo "error; you need to submit the form!";
+}
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$message = $_POST['message'];
+
+//Validate first
+if(empty($name)||empty($visitor_email)) 
+{
+    echo "Name and email are mandatory!";
+    exit;
+}
+
+if(IsInjected($visitor_email))
+{
+    echo "Bad email value!";
+    exit;
+}
+
+$email_from = 'info@apiscore.be';
+$email_subject = "Nouveau contact";
+$email_body = "Vous avez recu une nouvelle demande de $name.\n".
+    "Voici le message:\n $message \r\n".
+    
+$to = "nicolas@locla.be";//<== update the email address
+$headers = "From: $email_from \r\n";
+$headers .= "Reply-To: $visitor_email \r\n";
+//Send the email!
+if(mail($to,$email_subject,$email_body,$headers))
+  echo "2"; //done
+else
+  echo "1";//failure
+
+
+// Function to validate against any email injection attempts
+function IsInjected($str)
+{
+  $injections = array('(\n+)',
+              '(\r+)',
+              '(\t+)',
+              '(%0A+)',
+              '(%0D+)',
+              '(%08+)',
+              '(%09+)'
+              );
+  $inject = join('|', $injections);
+  $inject = "/$inject/i";
+  if(preg_match($inject,$str))
+    {
+    return true;
+  }
+  else
+    {
+    return false;
+  }
+}
+   
+?> 
