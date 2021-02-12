924a767b3b629dcc95e1b1cdb7ed7b7ad0ade7c7
Nicolas Matton <nicolas@locla.be>
2021-02-12 15:55:43 +0100

    add example

---
+++ assets/php/example_localsettings.php
@@ -0,0 +1,8 @@
+<?php
+define('PASSWORD', 'pwd');    
+define('USERNAME', 'usn');
+define('DB_ADDR', '197.0.1.201');
+define('DB_NAME', 'dbname');
+define('DB_PORT', 9999);
+define('EMAIL','my@email.com')
+?>
